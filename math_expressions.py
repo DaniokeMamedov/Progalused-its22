530e5ea461f59f6e975b393ff6d3180574d451a6
Daniel <daniel.kudrjakov@voco.ee>
2022-11-02 10:52:45 -0700

    Loodud projekti .gitignore fail ulesanne math extensions

---
+++ math_expressions.py
@@ -0,0 +1,63 @@
+num_a = 5
+num_b = 2
+num_c = 6
+seconds =  4968
+
+
+sum = num_a + num_b
+print(f"{num_a} + {num_b} = {sum}")
+print("-----------------------------")
+
+difference = num_a - num_b
+print(f"{num_a} - {num_b} = {difference}")
+print("-----------------------------")
+
+division = num_a / num_b
+print(f"{num_a} / {num_b} = {division}")
+print("-----------------------------")
+
+multiple = num_a * num_b
+print(f"{num_a} * {num_b} = {multiple}")
+print("-----------------------------")
+
+power = num_a ^ num_b
+print(f"{num_a} ^ {num_b} = {power}")
+print("-----------------------------")
+
+reminder = num_a / num_b
+print(f"{num_a} / {num_b} = {reminder}")
+print("-----------------------------")
+
+average = num_a + num_b / 2
+print(f"({num_a} + {num_b}) / {2} = {average}")
+print("----------------------------------")
+
+radius = 3.5
+import math
+pindala = math.pi * (radius * radius)
+print(f"{math.pi} * ({radius} * {radius}) = {pindala}")
+print("---------------------------------")
+
+side_lenght = 5
+triangle_area = 1.73205080756887
+print(f"{triangle_area}")
+print("---------------------------------")
+
+discriminant = num_b**2-4*num_a*num_c
+print(f"{num_b} ** {2} -{4} * {num_a} * {num_c} = {discriminant}")
+print("---------------------------------")
+
+c = num_a**2 + num_b**2
+print(f"{num_a} ** {2} + {num_b} ** {2} = {c}")
+print("---------------------------------")
+
+b = num_c**2 - num_a**2
+print(f"{num_c} ** {2} - {num_a} ** {2} = {b}")
+print("---------------------------------")
+
+hours = seconds // 3600
+minutes = 1368 // 60
+print(f"{seconds} // {3600} = {hours}")
+print(f"{1368} // {60} = {minutes}")
+print("---------------------------------")
+
